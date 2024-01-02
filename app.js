060ca441973b01aac7ec1bc3833abe8b0de91129
Amir Dehestani <amira.dehestani@gmail.com>
2024-01-02 14:53:41 -0500

    Connect to mongodb databse with mongoose

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
+mongooseConnect().catch((err) => console.log(err));
+
+async function mongooseConnect() {
+  await mongoose.connect(MONGODB_URI);
+
+  const cardSchema = new mongoose.Schema({
+    term: String,
+    definition: String,
+  });
+
+  const Card = mongoose.model('Card', cardSchema);
+
+  const exampleCard = new Card({ term: 'a', definition: 'one' });
+  await exampleCard.save();
+}
+
+console.log(`Connected to ${MONGODB_URI}`);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
