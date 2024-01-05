99faccdfdcb65c33893882e8befbb0491be41a34
Amir Dehestani <amira.dehestani@gmail.com>
2024-01-05 16:43:56 -0500

    Hide definitions by default, create reveal definition button

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,10 +1,23 @@
 import '../styles/Card.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { useState } from 'react';
 
 const Card = ({ flashcard }) => {
+  const [showDefinition, setShowDefinition] = useState(false);
   return (
     <div className="card">
       <div className="term">{flashcard.term}</div>
-      <div className="definition">{flashcard.definition}</div>
+      <div className="definition">
+        {showDefinition ? flashcard.definition : ''}
+      </div>
+      <FontAwesomeIcon
+        onClick={() => {
+          setShowDefinition(!showDefinition);
+        }}
+        className="fa-icon"
+        icon={icon({ name: 'eye-slash', family: 'classic', style: 'regular' })}
+      />
     </div>
   );
 };
--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -4,6 +4,10 @@ import '../styles/Cards.css';
 const Cards = ({ flashcards }) => {
   return (
     <div className="cards">
+      <div className="cards-header">
+        <div className="term-header">Term</div>
+        <div className="definition-header">Definition</div>
+      </div>
       {flashcards.map((flashcard) => (
         <Card className="card" key={flashcard._id} flashcard={flashcard} />
       ))}
