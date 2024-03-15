cfc5fef4e5673708842c5a4bad9c898ff09637ad
Amir Dehestani <amira.dehestani@gmail.com>
2024-03-15 19:01:00 -0400

    Add log out functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Navbar.css';
+import axios from 'axios';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem('accessToken');
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    delete axios.defaults.headers.common['Authorization'];
+    navigate('/');
+  };
+
   return (
     <div className="navbar">
       <Link className="nav-link" to="/">
         Home
       </Link>
-      <Link className="nav-link" to="/login">
-        Login
-      </Link>
-      <Link className="nav-link" to="/register">
-        Register
-      </Link>
+      {accessToken ? (
+        <>
+          <Link className="nav-link" onClick={handleLogout}>
+            Log out
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </>
+      )}
     </div>
   );
 };
